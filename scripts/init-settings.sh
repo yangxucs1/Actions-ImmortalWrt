b93fe8f18247bef4a4b8a3c36c45193b2a8ce163
yangxucs1 <35906909+yangxucs1@users.noreply.github.com>
2025-08-18 19:19:31 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#更改默认地址为192.168.123.1
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # 给config下的文件增加权限
 chmod 644 files/etc/config/*
